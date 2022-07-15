32d665ba35d27b13221aafece8ee4ee7e823f8e5
earthwormjim2 <robert.fritz@rmfhome.com>
2022-07-15 17:16:27 -0300

    added animation... had to wrap router to get location props

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,28 +6,27 @@ import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-
-
 import { Routes, Route, useParams, useLocation, useNavigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { postComment, fetchComments, fetchDishes, fetchPromos, resetFeedbackForm, updateFeedbackForm } from '../redux/ActionCreators';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 
 const mapStateToProps = state => {
     return {
         dishes: state.dishes,
-        comments: state.comments, 
+        comments: state.comments,
         promotions: state.promotions,
         leaders: state.leaders,
         feedback: state.feedback
-        }
+    }
 }
-    
+
 const mapDispatchToProps = (dispatch) => ({
-    postComment: (dishId, rating, author, comment) => {dispatch(postComment(dishId, rating, author, comment))},
+    postComment: (dishId, rating, author, comment) => { dispatch(postComment(dishId, rating, author, comment)) },
     fetchDishes: () => { dispatch(fetchDishes()) },
-    resetFeedbackForm: () => { dispatch(resetFeedbackForm()) } ,
-    updateFeedbackForm: (field, value) => { dispatch(updateFeedbackForm(field, value))},
+    resetFeedbackForm: () => { dispatch(resetFeedbackForm()) },
+    updateFeedbackForm: (field, value) => { dispatch(updateFeedbackForm(field, value)) },
     fetchComments: () => { dispatch(fetchComments()) },
     fetchPromos: () => { dispatch(fetchPromos()) }
 
@@ -48,6 +47,9 @@ function withRouter(Component) {
 
     return ComponentWithRouterProp;
 }
+
+const RoutedTransitionGroup = withRouter(CSSTransition);
+    
 class Main extends Component {
 
     componentDidMount() {
@@ -61,7 +63,7 @@ class Main extends Component {
             return (
                 <Home
                     dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
-                    promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]} 
+                    promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
                     dishesLoading={this.props.dishes.isLoading}
                     dishesErrMess={this.props.dishes.errMess}
                     promosLoading={this.props.promotions.isLoading}
@@ -79,27 +81,33 @@ class Main extends Component {
                     errMess={props.dishes.dishesErrMess}
                     comments={props.comments.comments.filter((comment) => comment.dishId === parseInt(dishId, 10))}
                     commentsErrMess={props.comments.comments.ErrMess}
-                    postComment={ props.postComment} />
+                    postComment={props.postComment} />
             );
         };
 
         // react router v6 requires component => element and wrap Component names, see: https://reactrouter.com/docs/en/v6/components/route 
         // Also, using React Hooks to pass parameters, current supported method in React Router 6, see: https://reactrouter.com/docs/en/v6/hooks/use-params
+        // Finally, using "RoutedTransition Group" to wraps CSSTransitionGroup... which adds props.router.location to the wrapped component,
+        // as Router no longer passes props.location by default.
         return (
             <div>
                 <Header />
-                <Routes>
-                    <Route path="/" index element={<HomePage />} />
-                    <Route path='/home' element={<HomePage dishes={this.props.dishes} />} />
-                    <Route path='/aboutus' element={<About leaders={this.props.leaders} />} />
-                    <Route path='/menu' element={<Menu dishes={this.props.dishes} />}/>
-                    <Route path='/menu/:dishId' element=
-                        {<DishWithId dishes={this.props.dishes} comments={this.props.comments}
-                            addComment={this.props.addComment} postComment={this.props.postComment} />} />
-                    <Route path="/contactus" element={<Contact resetFeedbackForm={this.props.resetFeedbackForm}
-                        updateFeedbackForm={this.props.updateFeedbackForm} />} />
-                    <Route path='*' element={<HomePage />} />
-                </Routes>
+                <TransitionGroup>
+                    <RoutedTransitionGroup key={this.props.router.location.key} classNames="page" timeout={300}> 
+                        <Routes location={this.props.router.location} >
+                            <Route path="/" index element={<HomePage />} />
+                            <Route path='/home' element={<HomePage dishes={this.props.dishes} />} />
+                            <Route path='/aboutus' element={<About leaders={this.props.leaders} />} />
+                            <Route path='/menu' element={<Menu dishes={this.props.dishes} />} />
+                            <Route path='/menu/:dishId' element=
+                                {<DishWithId dishes={this.props.dishes} comments={this.props.comments}
+                                    addComment={this.props.addComment} postComment={this.props.postComment} />} />
+                            <Route path="/contactus" element={<Contact resetFeedbackForm={this.props.resetFeedbackForm}
+                                updateFeedbackForm={this.props.updateFeedbackForm} />} />
+                            <Route path='*' element={<HomePage />} />
+                        </Routes>
+                    </RoutedTransitionGroup>
+                </TransitionGroup>
                 <Footer />
             </div>
         );
